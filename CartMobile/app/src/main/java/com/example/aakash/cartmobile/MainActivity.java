42851166c48d0ff9de7d7db09203f838bd8c56b7
CodeSlayer101 <33838368+CodeSlayer101@users.noreply.github.com>
2018-02-17 01:12:48 +0530

    SmartCart

---
+++ CartMobile/app/src/main/java/com/example/aakash/cartmobile/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.aakash.cartmobile;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.vision.barcode.Barcode;
+
+public class MainActivity extends Activity {
+    private Button mButton;
+    private static final int RC_BARCODE_CAPTURE = 9001;
+    private static final String TAG = "BarcodeMain";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void switchActivity(View v) {
+        Intent intent = new Intent(this, BarcodeScannerActivity.class);
+        startActivityForResult(intent, RC_BARCODE_CAPTURE);
+        intent.putExtra(BarcodeScannerActivity.AutoFocus, true);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RC_BARCODE_CAPTURE) {
+            if (resultCode == CommonStatusCodes.SUCCESS) {
+                if (data != null) {
+                    Barcode barcode = data.getParcelableExtra(BarcodeScannerActivity.BarcodeObject);
+                    Intent next = new Intent(this,ShoppingList.class);
+                    next.putExtra("Barcode",barcode.displayValue);
+                    startActivity(next);
+                    Toast.makeText(this,barcode.displayValue,Toast.LENGTH_LONG).show();
+                    Log.d(TAG, "Barcode read: " + barcode.displayValue);
+                } else {
+                    Toast.makeText(this, "Barcode Failed!", Toast.LENGTH_LONG).show();
+                    Log.d(TAG, "No barcode captured, intent data is null");
+                }
+            } else {
+                Toast.makeText(this, "Barcode error!", Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+}
